d6f75939410d79f2f627e2d8ea6b620d6b544e8c
robson.junior <robson.ga.junior@gmail.com>
2018-02-18 11:43:03 -0300

    [*] bugfix to install identify darwin/linux (bash start file)

--- install.js
+++ install.js
@@ -1,44 +1,52 @@
 #!/usr/bin/env node
 const fs = require('fs')
 const path = require('path')
+const os = require('os');
 const colors = require('./colors')
 const execSync = require('child_process').execSync
-const HOME = require('os').homedir()
-const PROFILE_PATH = HOME + path.sep + '.bash_profile'
+const HOME = os.homedir()
+const PLATFORM = os.platform();
+const PROFILE_PATH = HOME + path.sep + (PLATFORM === 'darwin' ? '.bash_profile' : '.bashrc')
 const writeInBox = require('./box.js')
 
 const redirectSH = __dirname + path.sep + 'redirect.sh'
-let SOURCE_COMMAND = '\n  source ' + redirectSH
+let SOURCE_COMMAND = 'source ' + redirectSH
 let SOURCE_COMMAND_STR = `
-# Pointing bash_profile to load profiler js${SOURCE_COMMAND};
+# Pointing ${PROFILE_PATH} to load profiler js\n${SOURCE_COMMAND};
 `
 
 try {
-    // if the user's bash_profile does not import our "redirect.sh" yet,
+    // if the user's .bash_profile/.bashrc file,
+    // does not import our "redirect.sh" yet,
     // we will append a line to it
     const bashProfileContent = fs.readFileSync(PROFILE_PATH, 'utf8').toString()
-    if (bashProfileContent.indexOf(SOURCE_COMMAND) < 0) {
-
+    let isAlreadyAddedCmd =  bashProfileContent.indexOf(SOURCE_COMMAND) < 0;
+    
+    if (isAlreadyAddedCmd) {
         execSync(`echo "${SOURCE_COMMAND_STR}" >> ${PROFILE_PATH}`)
-
-        const l = Math.max(60, SOURCE_COMMAND.length + 2)
-        console.log('\n > New source created and added to your ~/.bash_profile\n\n')
+        console.log(`\n> ${SOURCE_COMMAND} command added to your ${PROFILE_PATH} file\n\n`);
     } else {
         // let's uncomment the lines in redirect.sh
         let redirectContent = fs.readFileSync(redirectSH, 'utf8')
         redirectContent = redirectContent.replace(/(^|\n)\# /g, '$1')
+
         fs.writeFileSync(redirectSH, redirectContent, 'utf8')
     }
 } catch (error) {
-    if (error.message.toLowerCase().indexOf('denied') < 0) {
-        // if the problem was related to permission, we have to inform the user
+    let errorMessage = error.message.toLowerCase();
+    let isErrorDenied = errorMessage.indexOf('denied') < 0;
+
+    if (isErrorDenied) {
+        // if the problem was related to permission, 
+        // we have to inform the user
+
         console.log(
-            colors.red('[x] '),
-            'Permission denied to access ~/.bash_profile.\n' +
-            'You will need to add it to your bash_profile yourself: \n\n    ' +
+            colors.red(`[x] `),
+            `Permission denied to access ${PROFILE_PATH}.\n` +
+            `You will need to add it to your ${PROFILE_PATH} yourself: \n\n    ` +
             writeInBox(`\necho "${SOURCE_COMMAND.replace(/\n +/, '')}" >> ${PROFILE_PATH}`).join('\n    ') +
-            '\n' + 
-            '\nIt\'s required only once, after that, termtools takes care of it for you.'
+            `\n` + 
+            `\nIt\'s required only once, after that, termtools takes care of it for you.`
         )
     }
 }
